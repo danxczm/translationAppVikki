7e6d73a22f5c2c2e032b823fb8cbbdbdfa184ec1
dan <81313913+danxczm@users.noreply.github.com>
2023-12-04 12:35:46 +0200

    small fixes

--- src/WordListFunctionality/WordListFunctionality.jsx
+++ src/WordListFunctionality/WordListFunctionality.jsx
@@ -54,14 +54,14 @@ const WordListFunctionality = () => {
                 </span>
             </p>
 
-            <div className="ml-auto flex items-center justify-center">
+            <div className="ml-auto flex items-center justify-center bg-blue-600 rounded-md">
                 <div className="group relative">
                     <button
                         type="button"
                         disabled={data.length === 0}
                         onClick={addCollectionFireBase}
                         title="Save collection."
-                        className="items-center justify-center p-3 bg-blue-600 hover:bg-blue-700 text-white text-xl font-medium rounded-md disabled:bg-gray-300 disabled:cursor-not-allowed"
+                        className="items-center justify-center p-3 bg-blue-600 hover:bg-blue-700 text-white text-xl font-medium rounded-l-md disabled:cursor-not-allowed"
                     >
                         <HiOutlineSave size="20px" />
                     </button>
@@ -76,7 +76,7 @@ const WordListFunctionality = () => {
                         disabled={data.length === 0}
                         onClick={sortCollectionHandler}
                         title="Sort collection from A-Z."
-                        className="ml-1 items-center justify-center p-3 bg-blue-600 hover:bg-blue-700 text-white text-xl font-medium rounded-md disabled:bg-gray-300 disabled:cursor-not-allowed"
+                        className="ml-1 items-center justify-center p-3 bg-blue-600 hover:bg-blue-700 text-white text-xl font-medium disabled:cursor-not-allowed"
                     >
                         <HiOutlineSortDescending size="20px" />
                     </button>
@@ -90,7 +90,7 @@ const WordListFunctionality = () => {
                         disabled={data.length === 0}
                         onClick={clearDataFireBase}
                         title="Clear collection."
-                        className="ml-1 items-center justify-center p-3 bg-blue-600 hover:bg-blue-700 text-white text-xl font-medium rounded-md disabled:bg-gray-300 disabled:cursor-not-allowed"
+                        className="ml-1 items-center justify-center p-3 bg-blue-600 hover:bg-blue-700 text-white text-xl font-medium rounded-r-md disabled:cursor-not-allowed"
                     >
                         <HiOutlineTrash size="20px" />
                     </button>
